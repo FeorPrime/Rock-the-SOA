a93c68cf73d78eea6860edb5d764a683d72c93ef
Victoria <lesnayapol@yandex.ru>
2018-07-13 15:07:15 +0300

    simple service contract added

---
+++ ExternalService/Contracts/IExternalService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExternalService.Contracts
+{
+    interface IExternalService
+    {
+        string getSimpleData();
+
+        ComplexDataType getComplexData();
+
+        string getSimpleData(string simpleParameter);
+
+        ComplexDataType getComplexData(ComplexDataType complexParameter);
+    }
+
+    public class ComplexDataType
+    {
+        public string StingData { get; set; }
+        public int IntData { get; set; }
+    }
+}
